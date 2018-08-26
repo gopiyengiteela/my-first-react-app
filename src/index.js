af37b6482af06b18aebbee248b860da5a8cc6268
gopiyengiteela <gopi.yengiteela2@gmailcom>
2018-08-25 18:59:17 -0700

    Empty tic-tac-toe

--- src/index.js
+++ src/index.js
@@ -1,8 +1,67 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class Square extends React.Component{
+    render(){
+        return(
+        <button className="square">
+            {/*TODO*/}
+        </button>);
+    }
+}
+
+class Board extends React.Component{
+    renderSuqre(i){
+        return <Square />;
+    }
+
+    render(){
+        const status= "Next player: X";
+
+        return(
+            <div>
+                <div className="status">
+                    {status}
+                </div>
+                <div className="board-row">
+                    {this.renderSuqre(0)}
+                    {this.renderSuqre(1)}
+                    {this.renderSuqre(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSuqre(3)}
+                    {this.renderSuqre(4)}
+                    {this.renderSuqre(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSuqre(6)}
+                    {this.renderSuqre(7)}
+                    {this.renderSuqre(8)}
+                </div>
+            </div>
+        );
+    }
+}
+
+class Game extends React.Component{
+    render(){
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board />
+                </div>
+                <div className="game-ifo">
+                    <div>{/* status */}</div>
+                    <ol>{/* TODO */}</ol>
+                </div>
+            </div>
+
+        );
+    }
+}
+
+ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+);
